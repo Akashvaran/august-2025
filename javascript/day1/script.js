63fa3fd0498547ec48388e18f3dc67afbf3a81a8
Akashvaran <akash09052003@gmail.com>
2025-08-21 10:07:58 +0530

    code update

---
+++ javascript/day1/script.js
@@ -0,0 +1,59 @@
+// var name="akash";
+// var number=20;
+// var arr=["akash",20];
+// var num=20.3;
+
+// console.log(name)
+// console.log(number,num)
+
+// var name="akash"
+// console.log(name)
+
+// var name="arun "
+
+// console.log(name)
+
+
+// let name="manumaran"
+
+// let name="akash varaan"
+
+// const a=20;
+// const a=30;
+
+// var a=5;
+
+// a=20;
+// console.log(a);
+
+
+// let age=20;
+
+// age=350;
+
+// console.log(age);
+
+// const n=20;
+// n=30;
+
+// console.log(n)
+
+
+// function value(){
+//     var age=1;
+
+//     if(age){
+
+//         const Uname= "akash varan"
+//         console.log(age)
+//     }
+
+//     console.log(Uname)
+// }
+// value()
+// console.log(Uname)
+
+console.log(age);
+
+
+
